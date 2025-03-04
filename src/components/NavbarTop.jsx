d484baca186e89b8580f1333517ab8939bcbe035
Mansir <mansiraziz93@gmail.com>
2025-03-04 00:15:29 +0000

    `Removed NavProfile component and replaced it with UserNavProfile in NavbarTop component`

--- src/components/NavbarTop.jsx
+++ src/components/NavbarTop.jsx
@@ -5,8 +5,8 @@ import React, { useState } from "react";
 import { usePathname } from "next/navigation";
 import { Button } from "@heroui/react";
 import { useSession } from "next-auth/react";
-import NavProfile from "./NavProfile";
 import ThemeToggleButton from "./ThemeToggleButton";
+import UserNavProfile from "./UserNavProfile";
 
 export default function Navbar() {
   const { data: session, status } = useSession();
@@ -23,8 +23,6 @@ export default function Navbar() {
 
   const menuItems = [
     { name: "Home", link: "/" },
-    { name: "market", link: "/market" },
-    { name: "stake", link: "/#stake" },
   ];
 
   const menuList = (
@@ -60,49 +58,53 @@ export default function Navbar() {
           </Button>
         </>
       ) : (
-        <NavProfile />
+        <>
+          <UserNavProfile />
+        </>
       )}
-      <ThemeToggleButton/>
+      <ThemeToggleButton />
     </>
   );
 
   return (
-    <header
-      className={`top-0 fixed z-50 w-full bg-foreground primary text-copy `}
-    >
-      <div className="container mx-auto flex items-center justify-between p-4">
-        {/* Logo */}
-        <div className="lg:text-2xl text-base font-bold">
-          <a href="/">Crypto P@P</a>
-        </div>
-
-        {/* Desktop Menu */}
-        <nav className="hidden space-x-6 md:flex justify-center items-center">
-          {menuList}
-        </nav>
-
-        {/* Mobile Menu Button */}
-        <button
-          className="block text-2xl md:hidden"
-          onClick={() => setIsMenuOpen(!isMenuOpen)}
-          aria-label="Toggle Menu"
-        >
-          {isMenuOpen ? "✖" : <FaBars />}
-        </button>
-      </div>
-
-      {/* Mobile Menu Overlay */}
-      {isMenuOpen && (
-        <div
-          id="mobile-menu-overlay"
-          className="z-50 fixed inset-0 flex flex-col items-center justify-center space-y-6 bg-gray-900 bg-opacity-90 text-center text-2xl md:hidden"
-          onClick={handleOverlayClick}
-        >
-          <div className="flex flex-col justify-center items-center gap-5 ">
-            {menuList}
+    <>
+      <header
+        className={`top-0 fixed z-50 w-full bg-foreground primary text-copy `}
+      >
+        <div className="container mx-auto flex items-center justify-between p-4">
+          {/* Logo */}
+          <div className="lg:text-2xl text-base font-bold">
+            <a href="/">Crypto P@P</a>
           </div>
+
+          {/* Desktop Menu */}
+          <nav className="hidden space-x-6 md:flex justify-center items-center">
+            {menuList}
+          </nav>
+
+          {/* Mobile Menu Button */}
+          <button
+            className="block text-2xl md:hidden"
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
+            aria-label="Toggle Menu"
+          >
+            {isMenuOpen ? "✖" : <FaBars />}
+          </button>
         </div>
-      )}
-    </header>
+
+        {/* Mobile Menu Overlay */}
+        {isMenuOpen && (
+          <div
+            id="mobile-menu-overlay"
+            className="z-50 fixed inset-0 flex flex-col items-center justify-center space-y-6 bg-gray-900 bg-opacity-90 text-center text-2xl md:hidden"
+            onClick={handleOverlayClick}
+          >
+            <div className="flex flex-col justify-center items-center gap-5 ">
+              {menuList}
+            </div>
+          </div>
+        )}
+      </header>
+    </>
   );
 }
